9d40576a8f4f596e28baee2051553d8ad373ee76
Nikola <nicisekulovski@gmail.com>
2023-04-02 20:14:57 +0000

    Update code

--- api/index.js
+++ api/index.js
@@ -1,25 +1,35 @@
 const express = require("express");
 const cors = require("cors");
+//const https = require('https');
 const app = express();
-
 const jwt = require('jsonwebtoken');
 const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = require("./models/User.js");
+const cookieParser = require('cookie-parser');
+
+/*const fs = require('fs');
+const key = fs.readFileSync('../key.pem');
+const cert = fs.readFileSync('../cert.pem');*/
 require("dotenv").config();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
 const jwtSecret = 'random_string';
 
 app.use(express.json());
+app.use(cookieParser());
 
+/*const server = https.createServer({key: key, cert: cert }, app);*/
+
+/*
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://212.101.137.119:5175');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  //res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 
   next();
 });
+*/
 
 app.use(
   cors({
@@ -64,9 +74,12 @@ app.post("/login", async (req, res) => {
   if(userDoc){
     const passOk = bcrypt.compareSync(password, userDoc.password);
     if(passOk){
-      jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err, token)=>{
+      jwt.sign({
+        email:userDoc.email, 
+        id:userDoc._id,
+         name: userDoc.name}, jwtSecret, {}, (err, token)=>{
         if (err) throw err;
-        res.cookie('token', token, {sameSite:'none', secure: true}).json('pass ok');
+        res.cookie('token', token, {sameSite:'none', secure: false}).json(userDoc);
       });
       
     }
@@ -80,6 +93,21 @@ app.post("/login", async (req, res) => {
 });
 
 
+
+app.get('/profile', (req, res) =>{
+  const {token} = req.cookies;
+  if(token){
+    jwt.verify(token, jwtSecret, {}, async (err, userData) =>{
+      if(err) throw err;
+      const {name, email, id} = await User.findById(userData.id);
+      res.json({name, email, id});
+    })
+  }
+  else{
+    res.json(null);
+  }
+  
+})
 //F9TUCc0PNl801xrm
 
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,18 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 import Layout from "./Layout";
 import axios from "axios";
+import { UserContextProvider } from "./UserContext";
+
 
 axios.defaults.baseURL = "http://212.101.137.119:5000";
 axios.defaults.withCredentials = true;
 
 function App() {
+
+      
+
   return (
+    <UserContextProvider>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<IndexPage />} />
@@ -18,6 +24,7 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
       </Route>
     </Routes>
+    </UserContextProvider>
   );
 }
 
