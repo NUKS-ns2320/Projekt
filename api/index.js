09d4151891d0d7ba7d925e90e6b59fd397d375c0
Nikola <nicisekulovski@gmail.com>
2023-04-05 20:41:41 +0000

    it
    commit

--- api/index.js
+++ api/index.js
@@ -7,6 +7,9 @@ const bcrypt = require("bcryptjs");
 const mongoose = require("mongoose");
 const User = require("./models/User.js");
 const cookieParser = require('cookie-parser');
+const imageDownloader = require('image-downloader');
+const multer = require('multer');
+const fs = require('fs');
 
 /*const fs = require('fs');
 const key = fs.readFileSync('../key.pem');
@@ -18,6 +21,7 @@ const jwtSecret = 'random_string';
 
 app.use(express.json());
 app.use(cookieParser());
+app.use('/uploads', express.static(__dirname + '/uploads'));
 
 /*const server = https.createServer({key: key, cert: cert }, app);*/
 
@@ -115,6 +119,32 @@ app.post('/logout', (req, res) =>{
   res.cookie('token', '').json(true);
 });
 
+app.post('/upload-by-link', async (req, res) =>{
 
+  const {link} = req.body;
+  const newName = 'photo' + Date.now() + '.jpg';
+  await imageDownloader.image({
+    url:link,
+    dest: __dirname + '/uploads/' + newName,
+  });
+
+  res.json(newName);
+});
+
+const photosMiddleware = multer({dest: 'uploads/'});
+
+app.post('/upload',photosMiddleware.array('photos', 100), (req, res) => {
+  const uploadedFiles = [];
+  for (let i=0; i < req.files.length; i++){
+    const {path, originalname} = req.files[i];
+    const parts = originalname.split('.');
+    const ext = parts[parts.length -1];
+    const newPath = path + '.' + ext;
+    fs.renameSync(path, newPath);
+    uploadedFiles.push(newPath.replace('uploads/', ''));
+  }
+  res.json(uploadedFiles);
+
+});
 
 app.listen(5000);
--- client/src/pages/PlacesPage.jsx
+++ client/src/pages/PlacesPage.jsx
@@ -1,6 +1,9 @@
 import {Link, useParams} from "react-router-dom";
 import { useState } from "react";
 import Perks from '../Perks';
+import axios from "axios";
+import PhotosUploader from "../PhotosUploader";
+
 
 export default function PlacesPage (){
 
@@ -8,7 +11,6 @@ export default function PlacesPage (){
     const [title, setTitle] = useState('');
     const [address, setAddress] = useState('');
     const [addedPhotos, setAddedPhotos] = useState([]);
-    const [photoLink, setPhotoLink] = useState('');
     const [description, setDescription] = useState('');
     const [perks, setPerks] = useState([]);
     const [extrainfo, setExtraInfo] = useState('');
@@ -39,9 +41,7 @@ export default function PlacesPage (){
         );
     }
 
-    function addphotoByLink(){
-        
-    }
+    
 
     return(
 
@@ -77,24 +77,8 @@ export default function PlacesPage (){
 
                         {preInput('Photos', 'more = better')}
                     
-                        <div className="flex gap-2">
-                            <input
-                            value={photoLink} 
-                            onChange={ev => setPhotoLink(ev.target.value)}
-                            type="text" placeholder={"Add using a link ... jpg"}/>
-                            <button className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
-
-                        </div>
-                        <div className="grid gap-2  grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
-        
-                            <button className=" flex gap-1 justify-center border mt-2 border bg-transparent rounded-2xl p-8 text-2xl text-gray-600">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
-                                </svg>
-                                Upload
-                            </button>
-                        </div>
-
+                        <PhotosUploader addedPhotos={addedPhotos} onChange={setAddedPhotos}/>
+                        
                         {preInput('Description', 'Description of the place')}
                      
                         <textarea 
